e7f90f29782017a6ca4473d865df86c729d29dfe
balsmr <72560296+balsmr@users.noreply.github.com>
2022-08-22 16:58:33 +0200

    Update Tests.ps1

--- Tests.ps1
+++ Tests.ps1
@@ -1,12 +1,6 @@
-<#
-    .NOTES
-    Tests pour tester
-#>
-
+#Tests pour tester
 $nomhote = hostname
 $user = $env:USERNAME
-
 Write-Host -ForegroundColor Green " Bonjour $($user), bienvenue a Aperture Science"
 Write-Host -ForegroundColor Red "Il est l'heure de faire des tests sur " $nomhote
-
 Pause
