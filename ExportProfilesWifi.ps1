2e9505469482edacaf0a461979a7fa4c95e1ad36
balsmr <72560296+balsmr@users.noreply.github.com>
2022-08-22 11:34:21 +0200

    Add files via upload

---
+++ ExportProfilesWifi.ps1
@@ -0,0 +1,25 @@
+﻿#Dossier où sont exportés les profiles wifi
+$ProfileFolder = "c:\ProfilesWifi"
+
+#préparation liste pour les SSID
+$ssidList=@()
+#Liste des profiles brutes
+$RawList=netsh wlan show profiles
+ForEach($row1 in $RawList)
+{
+    #Récupération des ssids par expression régulière
+    If($row1 -match 'Profil Tous les utilisateurs[^:]+:.(.+)$')
+    {
+        $ssidList+=$Matches[1]
+
+    }
+}
+##Export des profiles wifi en xml
+#Test si le dossier existe sinon le créer 
+if((Test-Path $ProfileFolder) -eq $false){
+    New-Item -Path "c:\" -Name "ProfilesWifi" -ItemType directory | Out-Null
+}
+ForEach($ssid in $ssidList){
+    netsh wlan export profile $ssid key=clear folder=$ProfileFolder | Out-Null
+    Write-Host -ForegroundColor Green "Profile réseau Wifi $ssid exporté"
+    }
