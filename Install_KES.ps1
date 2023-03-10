c6e7f1ba1b0a1e68aae387c32224168772b6b668
balsmr <72560296+balsmr@users.noreply.github.com>
2023-03-10 17:18:49 +0100

    Update Install_KES.ps1

--- Install_KES.ps1
+++ Install_KES.ps1
@@ -1,13 +1,13 @@
-$KURL = 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576'
+#$KURL = 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576'
 #Path du fichier telechargé
-$Koutput = 'C:\Windows\temp\KES_TEST.exe'
+#$Koutput = 'C:\Windows\temp\KES_TEST.exe'
 #Arguments pour l'installation
-$KArgument = '/S /pEULA=1 /pPRIVACYPOLICY=1 /pKSN=1'
+#$KArgument = '/S /pEULA=1 /pPRIVACYPOLICY=1 /pKSN=1'
 
 #lancement du telechargement en utilisant basic parsing et la methode GET pour forcer l'attente de la fin du téléchargement avant de continuer
-Invoke-WebRequest $KURL -OutFile $Koutput -UseBasicParsing -Method Get 
+Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576' -OutFile 'C:\Windows\temp\KES_TEST.exe' -UseBasicParsing -Method Get 
 
 #on temporise 5 secondes avant de lancer l'instalaltion
 Start-Sleep -Seconds 5
 #on lance l'installaiton avec les arguments
-Start-Process -FilePath $Koutput -ArgumentList $KArgument
+Start-Process -FilePath 'C:\Windows\temp\KES_TEST.exe' -ArgumentList '/S /pEULA=1 /pPRIVACYPOLICY=1 /pKSN=1'
