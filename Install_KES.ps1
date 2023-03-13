b8953823c0a605b61e7d42510add23a0fedf7274
balsmr <72560296+balsmr@users.noreply.github.com>
2023-03-13 09:19:44 +0100

    sans job

--- Install_KES.ps1
+++ Install_KES.ps1
@@ -1,16 +1,7 @@
-
 #lancement du telechargement en utilisant basic parsing et la methode GET pour forcer l'attente de la fin du téléchargement avant de continuer
-Start-Job -Name Kdownload -ScriptBlock {
-    Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576' -OutFile 'C:\Windows\temp\KES_TEST.exe' -UseBasicParsing -Method Get 
-}
+Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576' -OutFile 'C:\Windows\temp\KES_TEST.exe' -UseBasicParsing -Method Get 
 
-while ((Get-Job -Name Kdownload).State -ne "Completed") {
-
-    Start-Sleep -Seconds 5
-    
-}
-
+#on temporise 5 secondes avant de lancer l'instalaltion
+Start-Sleep -Seconds 5
+#on lance l'installaiton avec les arguments
 Start-Process -FilePath 'C:\Windows\temp\KES_TEST.exe' -ArgumentList '/S /pEULA=1 /pPRIVACYPOLICY=1 /pKSN=1'
-
-
-Get-Job -Name Kdownload | where State -EQ "Completed" | Remove-Job
