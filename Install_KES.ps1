c4e485c70b8a5d4155c64f9799c21f213da94c55
balsmr <72560296+balsmr@users.noreply.github.com>
2023-03-13 15:49:21 +0100

    Update Install_KES.ps1

--- Install_KES.ps1
+++ Install_KES.ps1
@@ -1,5 +1,6 @@
 #lancement du telechargement en utilisant basic parsing et la methode GET pour forcer l'attente de la fin du téléchargement avant de continuer
-Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576' -OutFile 'C:\Windows\temp\KES_TEST.exe' -UseBasicParsing -Method Get 
+Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305' -OutFile 'C:\Windows\temp\KES_TEST.exe' -UseBasicParsing -Method Get 
+#https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576
 
 #on temporise 5 secondes avant de lancer l'instalaltion
 Start-Sleep -Seconds 5
