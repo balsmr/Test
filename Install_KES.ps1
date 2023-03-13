0c61e57e525e29b6e0f91a1e4a0b8feb24a6fcf7
balsmr <72560296+balsmr@users.noreply.github.com>
2023-03-13 15:16:45 +0100

    Update Install_KES.ps1

--- Install_KES.ps1
+++ Install_KES.ps1
@@ -4,4 +4,4 @@ Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576
 #on temporise 5 secondes avant de lancer l'instalaltion
 Start-Sleep -Seconds 5
 #on lance l'installaiton avec les arguments
-#Start-Process -FilePath 'C:\Windows\temp\KES_TEST.exe' -ArgumentList '/S /pEULA=1 /pPRIVACYPOLICY=1 /pKSN=1'
+Start-Process -FilePath 'C:\Windows\temp\KES_TEST.exe' -ArgumentList '-s'
