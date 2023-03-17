1ac1930afe5752c26dc6bff9248cc9ef6a4e7258
balsmr <72560296+balsmr@users.noreply.github.com>
2023-03-17 15:41:31 +0100

    Update Install_KES.ps1

--- Install_KES.ps1
+++ Install_KES.ps1
@@ -1,8 +1,9 @@
-﻿Set-ExecutionPolicy Unrestricted
+#lancement du telechargement en utilisant basic parsing et la methode GET pour forcer l'attente de la fin du téléchargement avant de continuer
+Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305' -OutFile 'C:\Windows\temp\KES_TEST.exe' -UseBasicParsing -Method Get 
+#https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13751576
 
-#on lance l'installation avec les arguments
+#on temporise 5 secondes avant de lancer l'instalaltion
+Start-Sleep -Seconds 5
+#on lance l'installaiton avec les arguments
 Start-Process -FilePath 'C:\Windows\temp\KES_TEST.exe' -ArgumentList '-s'
 
-
-
-Set-ExecutionPolicy restricted
