604f05cbe155cb8c8f4c7878c938d4a3c5e90252
balsmr <72560296+balsmr@users.noreply.github.com>
2023-03-17 15:56:49 +0100

    Update Install_KES_elevation.ps1

--- Install_KES_elevation.ps1
+++ Install_KES_elevation.ps1
@@ -1,7 +1,8 @@
 ﻿Set-ExecutionPolicy Unrestricted
 
 #on lance l'installation avec les arguments
-Start-Process -FilePath 'C:\Windows\temp\KES_TEST.exe' -ArgumentList '-s'
+#Start-Process -FilePath 'C:\Windows\temp\KES_TEST.exe' -ArgumentList '-s'
+Start-Process -FilePath "C:\Windows\temp\KES_TEST.exe" -ArgumentList "/S /pEULA=1 /pPRIVACYPOLICY=1 /pKSN=1" -Wait
 
 
 
