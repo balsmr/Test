550f645a9dd7238d2f208abc19c62b5a77d7df53
balsmr <72560296+balsmr@users.noreply.github.com>
2023-03-28 13:28:24 +0200

    Add files via upload

---
+++ KES_Installer2.ps1
@@ -0,0 +1,15 @@
+﻿#lancement du telechargement en utilisant basic parsing et la methode GET pour forcer l'attente de la fin du téléchargement avant de continuer
+Invoke-WebRequest 'https://dl.groupeisagri.com/Troupo/FR-FR/IT/KES_installer.zip' -OutFile 'C:\KES_installer.zip' -UseBasicParsing -Method Get 
+
+
+#on temporise 5 secondes avant de lancer l'instalaltion
+Start-Sleep -Seconds 5
+
+
+#on temporise 5 secondes avant de décompresser
+Start-Sleep -Seconds 5
+Expand-Archive C:\KES_installer.zip -DestinationPath C:\
+
+
+#on lance l'installaiton avec les arguments
+Start-Process -FilePath 'C:\KES_installer.exe' -ArgumentList '-s'
