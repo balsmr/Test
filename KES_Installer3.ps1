b0985ba20c7848869809f448986e0c6d3cdeb893
balsmr <72560296+balsmr@users.noreply.github.com>
2023-03-30 08:04:47 +0200

    Update KES_Installer3.ps1

--- KES_Installer3.ps1
+++ KES_Installer3.ps1
@@ -1,10 +1,1 @@
-Import-Module BitsTransfer
-
-#lancement du telechargement en utilisant basic parsing et la methode GET pour forcer l'attente de la fin du téléchargement avant de continuer
-Start-BitsTransfer -Source 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305' -Destination 'C:\Windows\temp\KES_installer.exe'
-
-#on temporise 5 secondes avant de lancer l'instalaltion
-Start-Sleep -Seconds 5
-
-#on lance l'installaiton avec les arguments
-Start-Process -FilePath 'C:\Windows\temp\KES_installer.exe' -ArgumentList '-s'
+curl -L  https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305 --output c:\KES_installer.exe
