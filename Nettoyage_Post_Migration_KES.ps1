c39ab628f607bbfd76202802a2c0d5e7903a873f
balsmr <72560296+balsmr@users.noreply.github.com>
2023-04-19 13:53:14 +0200

    Update Nettoyage_Post_Migration_KES.ps1

--- Nettoyage_Post_Migration_KES.ps1
+++ Nettoyage_Post_Migration_KES.ps1
@@ -2,4 +2,5 @@ Remove-Item C:\KES_Installer.exe
 Remove-Item C:\KES_Installer2.exe
 Remove-Item C:\Windows\temp\KES_installer.exe
 Remove-Item C:\Windows\temp\KES_test.exe
+Remove_Item C:\Windows\temp\KES_TEST.exe 
 Remove-Item C:\Windows\temp\ -Recurse
