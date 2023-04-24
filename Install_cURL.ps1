f31048fd70da2854a53a696fa9eef442536ad87e
balsmr <72560296+balsmr@users.noreply.github.com>
2023-04-24 15:00:11 +0200

    Update Install_cURL.ps1

--- Install_cURL.ps1
+++ Install_cURL.ps1
@@ -7,25 +7,34 @@
 ##Auteur : bbo
 ##
 ##########
-#on s'assure que le TLS est activé
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#on test si curl est déjà présent 
+if((Test-Path "C:\Windows\System32\curl.exe") -eq $true){
 
-#On télécharge le zip de curl a la racine du c
-Invoke-WebRequest 'https://curl.se/windows/latest.cgi?p=win64-mingw.zip' -OutFile 'C:\Windows\temp\cURL.zip' -UseBasicParsing -Method Get 
+    Write-Verbose "cURL déjà installé"
+    break
+}else{
 
-#on temporise
-Start-Sleep -Seconds 5
+    #on s'assure que le TLS est activé
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-#on dézip l'archive
-Expand-Archive "C:\Windows\temp\cURL.zip" -DestinationPath "C:\Windows\temp\" 
+    #On télécharge le zip de curl a la racine du c
+    Invoke-WebRequest 'https://curl.se/windows/latest.cgi?p=win64-mingw.zip' -OutFile 'C:\Windows\temp\cURL.zip' -UseBasicParsing -Method Get 
 
-#On renome le dossier créé
-Rename-Item "c:\Windows\temp\curl-8.0.1_7-win64-mingw" "c:\Windows\temp\cURL"
+    #on temporise
+    Start-Sleep -Seconds 5
 
-#On copie l'executable ddns system32 pour le rendre natif
-Copy-Item -Path "c:\Windows\temp\cURL\bin\curl.exe" -Destination "C:\Windows\System32\curl.exe"
+    #on dézip l'archive
+    Expand-Archive "C:\Windows\temp\cURL.zip" -DestinationPath "C:\Windows\temp\" 
 
-#on installe le certificat pour pouvoir télécharger en https et ftps
-Import-Certificate -FilePath "C:\Windows\temp\cURL\bin\curl-ca-bundle.crt"-CertStoreLocation Root
+    #On renome le dossier créé
+    Rename-Item "c:\Windows\temp\curl-8.0.1_7-win64-mingw" "c:\Windows\temp\cURL"
 
-Remove-Item "c:\Windows\temp\cURL" -Recurse
+    #On copie l'executable ddns system32 pour le rendre natif
+    Copy-Item -Path "c:\Windows\temp\cURL\bin\curl.exe" -Destination "C:\Windows\System32\curl.exe"
+
+    #on installe le certificat pour pouvoir télécharger en https et ftps
+    Import-Certificate -FilePath "C:\Windows\temp\cURL\bin\curl-ca-bundle.crt"-CertStoreLocation Root
+
+    Remove-Item "c:\Windows\temp\cURL" -Recurse
+
+}
