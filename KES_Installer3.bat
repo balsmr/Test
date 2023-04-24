6989ea9db35b3e43ec9a234bf69baadf281f32f8
balsmr <72560296+balsmr@users.noreply.github.com>
2023-04-24 16:41:52 +0200

    Update and rename KES_Installer3.bat to KES_Installer3.ps1

--- KES_Installer3.bat
+++
@@ -1,1 +0,0 @@
-curl "https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305"  -o "C:\Windows\temp\KES_installer.exe" -k
---
+++ KES_Installer3.ps1
@@ -0,0 +1,1 @@
+cmd curl "https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305"  -o "C:\Windows\temp\KES_installer.exe" -k
