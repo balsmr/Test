3bc959eb7e2d42de6f14a0c5c2f41c9f4a6d179d
balsmr <72560296+balsmr@users.noreply.github.com>
2023-04-24 14:22:45 +0200

    Add files via upload

---
+++ Install_cURL.ps1
@@ -0,0 +1,31 @@
+##########
+##
+##Nom : Install_cURL.ps1
+##Description : pour installer la commande cURL sur les postes qui ne l'ont pas 
+##Emplacement : git
+##Date modification : 24/04/2023
+##Auteur : bbo
+##
+##########
+#on s'assure que le TLS est activé
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#On télécharge le zip de curl a la racine du c
+Invoke-WebRequest 'https://curl.se/windows/latest.cgi?p=win64-mingw.zip' -OutFile 'C:\Windows\temp\cURL.zip' -UseBasicParsing -Method Get 
+
+#on temporise
+Start-Sleep -Seconds 5
+
+#on dézip l'archive
+Expand-Archive "C:\Windows\temp\cURL.zip" -DestinationPath "C:\Windows\temp\" 
+
+#On renome le dossier créé
+Rename-Item "c:\Windows\temp\curl-8.0.1_7-win64-mingw" "c:\Windows\temp\cURL"
+
+#On copie l'executable ddns system32 pour le rendre natif
+Copy-Item -Path "c:\Windows\temp\cURL\bin\curl.exe" -Destination "C:\Windows\System32\curl.exe"
+
+#on installe le certificat pour pouvoir télécharger en https et ftps
+Import-Certificate -FilePath "C:\Windows\temp\cURL\bin\curl-ca-bundle.crt"-CertStoreLocation Root
+
+Remove-Item "c:\Windows\temp\cURL" -Recurse
