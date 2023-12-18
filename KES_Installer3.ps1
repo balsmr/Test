65a26ded0dda87e817055fd609f348418873da6e
balsmr <72560296+balsmr@users.noreply.github.com>
2023-12-18 17:40:05 +0100

    Update KES_Installer3.ps1

--- KES_Installer3.ps1
+++ KES_Installer3.ps1
@@ -1,1 +1,1 @@
-curl "https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305"  -o "C:\Windows\temp\KES_installer.exe" -k
+curl "https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=18788040"  -o "C:\Windows\temp\KES_installer.exe" -k
