d816b27d4db9f29556ec2665fdc116175a1f460a
balsmr <72560296+balsmr@users.noreply.github.com>
2023-12-18 17:39:25 +0100

    Update Install_KES.ps1

--- Install_KES.ps1
+++ Install_KES.ps1
@@ -2,7 +2,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 #lancement du telechargement en utilisant basic parsing et la methode GET pour forcer l'attente de la fin du téléchargement avant de continuer
-Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305' -OutFile 'C:\Windows\temp\KES_installer.exe' -UseBasicParsing -Method Get 
+Invoke-WebRequest 'https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=18788040' -OutFile 'C:\Windows\temp\KES_installer.exe' -UseBasicParsing -Method Get 
 
 #on temporise 5 secondes avant de lancer l'instalaltion
 Start-Sleep -Seconds 5
