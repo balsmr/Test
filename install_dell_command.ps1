acb9be0294af0ab44069e58e3fecbf711e82aaaf
balsmr <72560296+balsmr@users.noreply.github.com>
2024-07-08 14:45:14 +0200

    Update install_dell_command.ps1

--- install_dell_command.ps1
+++ install_dell_command.ps1
@@ -1,5 +1,12 @@
-Set-ExecutionPolicy RemoteSigned
-
+# Check if running as administrator
+if (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    # If not running as administrator, restart this script as administrator
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    exit
+}
 
 # Install Dell Command Update silently
 winget install Dell.CommandUpdate --silent
+
