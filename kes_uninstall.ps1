1b41c9dd08c68de6c6bd7822b21a9a1716fe2822
balsmr <72560296+balsmr@users.noreply.github.com>
2025-03-11 17:28:49 +0100

    Update kes_uninstall.ps1

--- kes_uninstall.ps1
+++ kes_uninstall.ps1
@@ -1,4 +1,8 @@
-$app = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE 'Kaspersky%'"
-if ($app) {
-    $app.Uninstall()
+$KasperskyApp = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE 'Kaspersky%'"
+
+if ($KasperskyApp) {
+    $Password = "!H_<*i:6dJeU7HABbt(F"
+    $KLAdminUser = "KLAdmin"
+
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $($KasperskyApp.IdentifyingNumber) /qn REMOVE=ALL /pKLPASSWD=$Password /pKLLOGIN=$KLAdminUser" -Wait -NoNewWindow
 }
