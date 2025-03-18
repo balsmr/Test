259d49b209c611b499de02e5d99b1903d7f7446f
balsmr <72560296+balsmr@users.noreply.github.com>
2025-03-18 16:14:20 +0100

    Create Testssuppretnotification.ps1

---
+++ Testssuppretnotification.ps1
@@ -0,0 +1,95 @@
+##########
+##
+##Nom : SupprUninstaller.ps1
+##Description : script pour supprimer une application par son uninstaller silencieux si renseigné dans la base de registre
+##Emplacement : git / tache eset
+##Date modification : 18/03/2025
+##Auteur : bbo
+##
+##########
+
+#-------------------- Variables a modifier ---------------------#
+#Nom de l'application a chercher
+$Application = "OneSafe PC Cleaner"
+
+#--------------------- Fonctions utilisés ---------------------#
+function Send-notification {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Titre,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Message,
+
+        [Parameter()]
+        [ValidateSet('INFO', 'WARNING', 'ERROR', 'NONE')]
+        [string]$Icone = 'INFO',
+
+        [Parameter()]
+        [int]$Duree = 10000
+    
+    )
+
+    [reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+    [reflection.assembly]::loadwithpartialname('System.Drawing')
+    $notify = new-object system.windows.forms.notifyicon
+    $notify.icon = [System.Drawing.SystemIcons]::Information #A garder pour desiner icone, mais le choix de l'icone se fait dasn dernière ligne
+    $notify.visible = $true
+    $notify.showballoontip($Duree,$Titre,$Message,[system.windows.forms.tooltipicon]::$Icone)
+    
+}
+
+
+
+#--------------------- Script --------------------------------#
+# Chemin vers les clés de désinstallation dans le registre
+$path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$path64 = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+
+
+# Recherche de l'application
+$prog = Get-ChildItem -Path $path | ForEach-Object {
+    $appName = (Get-ItemProperty -Path $_.PSPath -Name DisplayName -ErrorAction SilentlyContinue).DisplayName
+    if ($appName -like "*$($Application)*") {
+        Get-ItemProperty -Path $_.PSPath
+    }
+}
+
+$prog64 = Get-ChildItem -Path $path64 | ForEach-Object {
+    $appName = (Get-ItemProperty -Path $_.PSPath -Name DisplayName -ErrorAction SilentlyContinue).DisplayName
+    if ($appName -like "*$($Application)*") {
+        Get-ItemProperty -Path $_.PSPath
+    }
+}
+
+# Vérifie si l'application a été trouvée
+if ($prog.QuietUninstallString) {
+    # Lance la commande de désinstallation silencieuse
+    Start-Process -FilePath "cmd.exe" -ArgumentList "/c $($prog.QuietUninstallString)" -Wait -NoNewWindow
+    Write-Output "Désinstallation lancée avec succès."
+} elseif ($prog64.QuietUninstallString) {
+    # Lance la commande de désinstallation silencieuse
+    Start-Process -FilePath "cmd.exe" -ArgumentList "/c $($prog64.QuietUninstallString)" -Wait -NoNewWindow
+    Write-Output "Désinstallation lancée avec succès."
+} else {
+    #Création de la requete qui vas servir pour la commande get-wmiobject
+    $requete = "SELECT * FROM Win32_Product WHERE Name LIKE '%" + $Application +"%'"
+    #On selectionne la ou les application(s) qui contienne le nom entre %
+    $AppGui = Get-WmiObject -Query $requete
+    
+    #Si la selection contient une ou des application(s)
+    if ($AppGui) {z
+         #Pour chaque application
+        foreach($app in $AppGui){
+            #On prépare les arguments (possibilite d'ajouter un id et mdp pour kapsersky par exemple)
+            $Argument = "/x $($app.IdentifyingNumber) /qn"
+            #on lance la commande MSIEXEC pour lancer la désinstallation 
+            Start-Process -FilePath "msiexec.exe" -ArgumentList $Argument -Wait -NoNewWindow
+    
+        }
+
+    }
+}
+Send-notification -titre "Test notifications" -message "message de test"
+
+
