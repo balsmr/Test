b5d085d6fec55c3ef07470d2f8ace8d3c840a17b
balsmr <72560296+balsmr@users.noreply.github.com>
2025-05-21 08:40:33 +0200

    Create Kes_uninstall-nopass-3.ps1

---
+++ Kes_uninstall-nopass-3.ps1
@@ -0,0 +1,8 @@
+$uninstallPath = "C:\Program Files (x86)\Kaspersky Lab\Kaspersky Endpoint Security for Windows\avp.com"
+$password = "!H_<*i:6dJeU7HABbt(F"  # Adapté si le mot de passe est requis
+
+if (Test-Path $uninstallPath) {
+    Start-Process -FilePath $uninstallPath -ArgumentList "REMOVE /silent" -Wait -NoNewWindow
+} else {
+    Write-Output "L'outil avp.com n'est pas trouvé sur ce poste."
+}
