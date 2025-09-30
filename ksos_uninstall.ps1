ce2ddd6f84706e70b252a9fac5df6199d35ab963
balsmr <72560296+balsmr@users.noreply.github.com>
2025-09-30 10:00:12 +0200

    Delete ksos_uninstall.ps1

--- ksos_uninstall.ps1
+++
@@ -1,8 +0,0 @@
-$KasperskyApp = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE '%Kaspersky Small%'"
-
-if ($KasperskyApp) {
-    $Password = "IT_K@5per5kY"
-    $KLAdminUser = "KLAdmin"
-    $Argument = "/x $($KasperskyApp.IdentifyingNumber) KLLOGIN=$KLAdminUser KLPASSWD=$Password /qn"
-    Start-Process -FilePath "msiexec.exe" -ArgumentList $Argument -Wait -NoNewWindow
-}
