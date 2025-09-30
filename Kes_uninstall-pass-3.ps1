0c81bd1374eeabe4e687a6307ff9e8c1370d7911
balsmr <72560296+balsmr@users.noreply.github.com>
2025-09-30 09:59:52 +0200

    Delete Kes_uninstall-pass-3.ps1

--- Kes_uninstall-pass-3.ps1
+++
@@ -1,8 +0,0 @@
-$uninstallPath = "C:\Program Files (x86)\Kaspersky Lab\Kaspersky Endpoint Security for Windows\avp.com"
-$password = "!H_<*i:6dJeU7HABbt(F"  # Adapté si le mot de passe est requis
-
-if (Test-Path $uninstallPath) {
-    Start-Process -FilePath $uninstallPath -ArgumentList "REMOVE /password=$password /silent" -Wait -NoNewWindow
-} else {
-    Write-Output "L'outil avp.com n'est pas trouvé sur ce poste."
-}
