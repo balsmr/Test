fef4d17d1eab11daef260a0ed44376095172cdf8
balsmr <72560296+balsmr@users.noreply.github.com>
2025-10-01 14:09:59 +0200

    Delete install_KESw8.ps1

--- install_KESw8.ps1
+++
@@ -1,5 +0,0 @@
-$Url = "https://kaspersky-smr.groupeisagri.com:8061/dlpkg?id=13717305"  
-$OutputPath = "C:\Windows\temp\KES_installer.exe"
-
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile($Url, $OutputPath)
